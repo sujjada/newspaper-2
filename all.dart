b0350b6bc3d027a0c2dab26990e708a84face070
Sujjada zabin <90233176+sujjada@users.noreply.github.com>
2021-11-16 12:55:08 +0600

    Add files via upload

---
+++ all.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:newspaper/screens/details.dart';
+
+class AllNews extends StatefulWidget {
+  static const String path = "AllNews";
+  const AllNews({Key? key}) : super(key: key);
+
+  @override
+  _AllNewsState createState() => _AllNewsState();
+}
+
+class _AllNewsState extends State<AllNews> {
+  final String url =
+      "https://newsapi.org/v2/everything?q=tesla&from=2021-10-16&sortBy=publishedAt&apiKey=4159422918ad47e1bca6d72a504c5da6";
+  List allNews = [];
+
+  Future getAllNews() async {
+    final response = await http.get(Uri.parse(url));
+    Map _allNews = {};
+    if (response.statusCode == 200) {
+      _allNews = jsonDecode(response.body);
+      setState(() {
+        allNews = _allNews['articles'];
+      });
+    }
+  }
+
+  String getAuthor(author) {
+    if (author != null) {
+      return author;
+    } else {
+      return "UnKnown Resource";
+    }
+  }
+
+  String getImage(image) {
+    if (image != null) {
+      return image;
+    } else {
+      return "Image Not Found";
+    }
+  }
+
+  @override
+  void initState() {
+    getAllNews();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: ListView.builder(
+          itemCount: allNews.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              elevation: 10,
+              child: ListTile(
+                onTap: () {
+                  Navigator.pushNamed(context, DetailScreen.path,
+                      arguments: allNews[index]);
+                },
+                isThreeLine: true,
+                title: Text(allNews[index]['title']),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(getAuthor(allNews[index]['author'])),
+                    Text(allNews[index]['publishedAt'])
+                  ],
+                ),
+                trailing: Hero(
+                    tag: "${allNews[index]['title']}",
+                    child: Image.network(
+                      getImage(allNews[index]['urlToImage']),
+                      height: 100,
+                      width: 80,
+                      fit: BoxFit.cover,
+                    )),
+              ),
+            );
+          }),
+    );
+  }
+}
---
+++ details.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class DetailScreen extends StatelessWidget {
+  static const String path = "DetailScreen";
+  const DetailScreen({Key? key}) : super(key: key);
+
+  Future launchUrl(url) async {
+    if (await canLaunch(url)) {
+      return launch(url);
+    } else {
+      throw "Could not launch $url";
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Map newsData = ModalRoute.of(context)!.settings.arguments as Map;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "News International",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notification_important,
+                color: Colors.black,
+              ))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            Text(
+              newsData["title"],
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            Hero(
+                tag: "${newsData["title"]}",
+                child: Image.network(
+                  newsData["urlToImage"],
+                  height: 250,
+                  width: double.infinity,
+                )),
+            SizedBox(
+              height: 8,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Author: ${newsData["author"]}"),
+                Text("Published: ${newsData["publishedAt"]}")
+              ],
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            RichText(
+                text: TextSpan(children: [
+              TextSpan(
+                text: newsData["description"],
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+              ),
+              TextSpan(
+                  text: "[more details]",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      launchUrl(newsData['url']);
+                    }),
+            ]))
+          ],
+        ),
+      ),
+    );
+  }
+}
